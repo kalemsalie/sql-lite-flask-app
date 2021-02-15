f328fe32752ad8eb2a6352d024b3882c5cfa8676
kalemsalie <kalemsalie999@gmail.com>
2021-02-15 14:53:13 +0200

    my first commit

---
+++ app.py
@@ -0,0 +1,62 @@
+import sqlite3
+from flask import Flask, render_template, request
+
+def init_sqlite_db():
+    conn = sqlite3.connect('database.db')
+    print("Opened database uccessfully")
+
+    conn.execute('CREATE TABLE IF NOT EXISTS students (name TEXT,addr TEXT,city TEXT,pin TEXT)')
+    print("Table created successfully")
+    conn.close()
+
+init_sqlite_db()
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route("/student.html/")
+def enter_new_student():
+    return render_template('student.html')
+
+@app.route('/add-new-record/', methods=['POST'])
+def add_new_record():
+    if request.method == 'POST':
+        try:
+            name = request.form['name']
+            addr = request.form['add']
+            city = request.form['city']
+            pin = request.form['pin']
+
+            with sqlite3.connect('database.db') as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO students (name,addr,city,pin)VALUES(?,?,?,?)",(name,addr,city,pin))
+                con.commit()
+                msg = "Record Successfully Added"
+        except Exception as e:
+            con.rollback()
+            msg = "Error occured in insert operation:" + e
+
+        finally:
+            con.close()
+            return render_template('result.html',msg=msg)
+
+@app.route('/show-records/',methods=["GET"])
+def show_records():
+    records = []
+    try:
+        with sqlite3.connect('database.db')as con:
+            cur = con.cursor()
+            cur.execute("SELECT * FROM students")
+            records = cur.fetchall()
+    except Exception as e:
+        con.rollback()
+        print("There was an error fetching results from the database.")
+    finally:
+        con.close()
+    return render_template('records.html',records=records)
+
+
+
+
+
+
